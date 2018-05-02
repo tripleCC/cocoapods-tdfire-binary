7890ee60c4fe87eb9713fb836bae70d398c3b417
tripleCC <triplec.linux@gmail.com>
2018-05-02 10:31:37 +0800

    添加 pod binary search 命令 #1

---
+++ lib/cocoapods-tdfire-binary/command/search.rb
@@ -0,0 +1,46 @@
+
+require 'json'
+require 'cocoapods-tdfire-binary/binary_url_manager'
+
+module Pod
+  class Command
+    class Binary < Command
+      class Search < Binary
+
+        self.abstract_command = false
+        self.summary = '查找二进制版本信息'
+        self.description = <<-DESC
+					查找二进制版本信息
+        DESC
+
+        self.arguments = [
+            CLAide::Argument.new('NAME', true)
+        ]
+
+        def initialize(argv)
+          @name = argv.shift_argument
+          super
+        end
+
+        def validate!
+          super
+          help! "必须指定有效组件名" if @name.nil?
+        end
+
+        def run
+          result = Pod::Tdfire::BinaryUrlManager.search_binary(@name)
+          result = JSON.parse(result)
+
+          name = result['name'] || @name
+          versions = result['versions'] || []
+
+          title = "-> #{name} (#{versions.last})".green
+
+          Pod::UI::title(title, '', 1) do
+            Pod::UI::labeled('Versions', versions.join(', '))
+          end
+        end
+      end
+    end
+  end
+end
