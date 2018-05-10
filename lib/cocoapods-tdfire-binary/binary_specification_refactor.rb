d418ed33fdae69cc02a0ae9c863292dc54d4760d
tripleCC <triplec.linux@gmail.com>
2018-05-10 10:33:59 +0800

    下载错误，让prepare_command执行失败

--- lib/cocoapods-tdfire-binary/binary_specification_refactor.rb
+++ lib/cocoapods-tdfire-binary/binary_specification_refactor.rb
@@ -1,3 +1,4 @@
+
 require 'cocoapods-tdfire-binary/binary_url_manager'
 require 'cocoapods-tdfire-binary/binary_state_store'
 
@@ -190,6 +191,7 @@ module Pod
 			def set_framework_download_script
 				download_url = Pod::Tdfire::BinaryUrlManager.pull_url_for_pod_version(target_spec.root.name, target_spec.version)
 
+        error_message = %Q["Tdfire: 请确认 #{target_spec.root.name}(#{target_spec.version}) 二进制版本是否存在!"].force_encoding('UTF-8')
 				download_script = <<-EOF
         #!/bin/sh
 
@@ -211,6 +213,11 @@ module Pod
 
           unzip #{framework_name}.zip
           cp -fa #{framework_name} ../
+        else
+          echo "\n!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!"
+          echo #{error_message}
+          echo "!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!\n"
+          exit 127
         fi
 
         cd ..
