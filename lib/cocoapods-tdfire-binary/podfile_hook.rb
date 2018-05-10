01fe7ed88a0d3a04c73c4d6e5c2a7722de233afe
青木 <qingmu@2dfire.com>
2018-05-10 17:50:43 +0800

    feat: 处理本地cache及本地目录Pods下的组件

--- lib/cocoapods-tdfire-binary/podfile_hook.rb
+++ lib/cocoapods-tdfire-binary/podfile_hook.rb
@@ -1,3 +1,5 @@
+require 'fileutils'
+require 'colored2'
 require 'cocoapods-tdfire-binary/binary_state_store'
 require 'cocoapods-tdfire-binary/source_chain_analyzer'
 require 'cocoapods-tdfire-binary/specification_dsl'
@@ -11,6 +13,8 @@ module Pod
 
 			specs = analysis_result.specifications.uniq { |s| s.root.name }
 			use_binary_specs = specs.reject(&:tdfire_use_source?)
+
+			# 1、先找出没有二进制版本的组件 
 			invalid_specs =  use_binary_specs.reject do |s|
 				json_string = Pod::Tdfire::BinaryUrlManager.search_binary(s.root.name)
 				pod = JSON.parse(json_string, object_class: OpenStruct)
@@ -18,35 +22,55 @@ module Pod
 				versions.include?(s.version.to_s)
 			end
 
+			# 2、判断有效组件的 cache 中是否有二进制，没有的话，删除组件缓存
 			valid_specs = use_binary_specs - invalid_specs
 			if valid_specs.any?
 				cache = Downloader::Cache.new(Config.instance.cache_root + 'Pods')
 				cache_descriptors = cache.cache_descriptors_per_pod
 
 				valid_specs.each do |s|
-					# Config.instance.sandbox
+					# 2.1、处理 Pods 
+					pod_dir = Config.instance.sandbox.pod_dir(s.root.name)
+					framework_file = pod_dir + "#{s.root.name}.framework"
+					if pod_dir.exist? && !framework_file.exist?
+						UI.message "Tdfire: 删除缺少二进制的组件 #{s.root.name}".yellow
+						begin
+							FileUtils.rm_rf(pod_dir)
+						rescue
+						end
+					end
 
-					puts s.root.name
+					# 2.2、处理 cache
 					descriptors = cache_descriptors[s.root.name]
-					
 					next if descriptors.nil?
 
-					descriptor = descriptors.select { |d| d[:version] == s.version }
-					next if descriptor.nil?
+					descriptors = descriptors.select { |d| d[:version] == s.version}
+					descriptors.each do |d|
 
-					framework_path = File.join(descriptor[:slug], "#{s.root.name}.framework")
-					puts framework_path
-					unless (File.directory?(framework_path))
-						# FileUtils.rm(descriptor[:spec_file])
-						# FileUtils.rm(descriptor[:slug])
+						# pod cache 文件名由文件内容的 sha1 组成，由于生成时使用的是 podspec，获取时使用的是 podspec.json 
+						# 导致生成的文件名不一致
+						# Downloader::Request slug
+						slug = d[:slug].dirname + "#{s.version}-#{s.checksum[0, 5]}"
+						framework_file = slug + "#{s.root.name}.framework"
+						unless (framework_file.exist?)
+							UI.message "Tdfire: 删除缺少二进制的Cache #{s.root.name}".magenta
+							begin
+								FileUtils.rm(descriptor[:spec_file])
+								FileUtils.rm_rf(descriptor[:slug])
+							rescue
+							end
+						end
 					end
 				end
 			end
 			
+			# 3、抛出二进制不存在异常
 			if invalid_specs.any?
 				specs_message = invalid_specs.map do |s|
 					"#{s.root.name} (#{s.version})"
 				end.uniq.join("\n")
+
+				UI.message "Tdfire: 添加以下代码至Podfile规避此错误: tdfire_use_source_pods #{invalid_specs.map { |s| s.root.name }}"
 				raise Informative, "以下组件没有二进制版本: \n#{specs_message} " 
 			end
 		end
