64086b7c58cd942fd0a6152f6481ce57037972ad
tripleCC <triplec.linux@gmail.com>
2018-05-17 17:31:24 +0800

    fix: undefined method [] for nil:NilClass #19

--- lib/cocoapods-tdfire-binary/command/init.rb
+++ lib/cocoapods-tdfire-binary/command/init.rb
@@ -23,7 +23,8 @@ module Pod
           hash = binary_config.setting_hash
 
           Tdfire::InitAsker::QUESTIONS.each do |k, v|
-            hash[k] = @asker.ask_with_answer(v, hash[k])
+            default = hash[k] unless hash.nil?
+            hash[k] = @asker.ask_with_answer(v, default)
           end
 
           binary_config.config_with_setting(hash)
